1bf779b847eed670cdb9b9156720213e74639381
atageldi194229 <atageldi194229@gmail.com>
2022-08-25 02:18:04 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/devatasan/duralga_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devatasan.duralga_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  await Hive.openLazyBox("appBox");
+
+  // runApp(const App());
+}
